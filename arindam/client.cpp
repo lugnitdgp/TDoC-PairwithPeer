8c549216025e1140f719c86cb772f5cbbc24caf6
arindam <ariyoucandoit@gmail.com>
2022-12-21 14:03:44 +0530

    full project

---
+++ arindam/client.cpp
@@ -0,0 +1,8 @@
+#include "client_handler.cpp"
+
+int main()
+{
+    RunClient();
+    
+    return 0;
+}
---
+++ arindam/server.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+//the server_handler file
+#include "server_handler.cpp"
+
+int main(int argc, char const *argv[])
+{
+    //Entry point for server
+    RunServer();
+    return 0;
+}
